ba36262758bd4b54de723643b839bf646c8f9c49
edsilv <e.silverton@gmail.com>
2015-07-17 10:15:30 +0100

    update manifesto paths

--- views/layout.jsx
+++ views/layout.jsx
@@ -11,18 +11,19 @@ var Layout = React.createClass({
             <head>
                 <title>{this.props.title}</title>
                 <link rel="stylesheet" href="/css/style.css" />
+                <script src="/lib/es5-shim/es5-shim.js"></script>
+                <script src="/lib/manifesto/dist/client/manifesto.js"></script>
+                <script src="/lib/jquery/dist/jquery.js"></script>
+                <script src="/js/scripts.js"></script>
                 <script dangerouslySetInnerHTML={{__html:`
-            // This is making use of ES6 template strings, which allow for
-            // multiline strings. We specified "{jsx: {harmony: true}}" when
-            // creating the engine in app.js to get this feature.
-            //console.log("hello world");
-          `}}/>
+                    // This is making use of ES6 template strings, which allow for
+                    // multiline strings. We specified "{jsx: {harmony: true}}" when
+                    // creating the engine in app.js to get this feature.
+                    //console.log("hello world");
+                  `}}/>
             </head>
             <body>
                 {this.props.children}
-                <script src="/lib/jquery/dist/jquery.js"></script>
-                <script src="/lib/manifesto/dist/manifesto.client.js"></script>
-                <script src="/js/scripts.js"></script>
             </body>
             </html>
         );
